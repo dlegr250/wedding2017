c9e6180fbf31abba1c7e88eb74b013e86b4d9413
Daniel LeGrand <dan.legrand@gmail.com>
2017-04-23 06:49:33 -0400

    Add guests list to dashboard page

--- app/controllers/authenticated/dashboards_controller.rb
+++ app/controllers/authenticated/dashboards_controller.rb
@@ -1,8 +1,11 @@
 module Authenticated
   class DashboardsController < BaseController
     before_action :set_page_title
-    
+
     def show
+      @attending = current_account.guests.attending.alphabetical.includes(:party)
+      @not_attending = current_account.guests.not_attending.alphabetical.includes(:party)
+      @not_rsvped = current_account.guests.not_rsvped.alphabetical.includes(:party)
     end
 
     private
